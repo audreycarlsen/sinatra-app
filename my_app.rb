436d16a65a7f236fbaa9d20e53b9a69394836ff1
Audrey Carlsen <audreycarlsen@gmail.com>
2013-11-25 16:42:37 -0800

    Added index page, print page

--- my_app.rb
+++ my_app.rb
@@ -5,7 +5,19 @@ class MyApp < Sinatra::Base
     erb :index
   end
 
-  get '/cute_pictures_of_animals' do
-    erb :cute_pictures_of_animals
+  get '/print' do
+    erb :print
+  end
+
+  get '/radio' do
+    erb :radio
+  end
+
+  get '/this_carleton_life' do
+    erb :this_carleton_life
+  end
+
+  get '/contact' do
+    erb :contact
   end
 end
