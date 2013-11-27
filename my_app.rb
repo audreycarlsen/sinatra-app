0dca82e9d4196fad269315a15c09f3cc7019974e
Audrey Carlsen <audreycarlsen@gmail.com>
2013-11-27 13:40:21 -0800

    Blog is sorta working

--- my_app.rb
+++ my_app.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 
 class MyApp < Sinatra::Base
+  before do
+    @posts = Dir.glob('views/posts/*.erb').map do |post_name|
+      post_name.split('/').last.slice(0..-5)
+    end
+  end
+
   get '/' do
     erb :index, :layout => false
   end
@@ -17,8 +23,12 @@ class MyApp < Sinatra::Base
     erb :this_carleton_life
   end
 
+  get '/blog' do
+    erb :blog, :layout => :blog_layout
+  end
+
   get '/blog/:post_name' do
-    erb "/posts/#{params[:post_name]}".to_sym
+    erb "/posts/#{params[:post_name]}".to_sym, :layout => :blog_layout
   end
 
   get '/about' do
